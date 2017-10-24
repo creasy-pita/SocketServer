7e6c12cd6b897316f26d2fe1db41f2288a6303fe
creasy-pita <hz_ljq@live.com>
2017-10-24 17:42:54 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Text;  
+
+namespace SocketServer
+{
+    class Program
+    {
+        private static byte[] result = new byte[1024];
+        private static int myProt = 8885;   //端口  
+        static Socket serverSocket;
+        static void Main(string[] args)
+        {
+            StringServer.InitSocketServer();
+
+            ////服务器IP地址  
+            //IPAddress ip = IPAddress.Parse("127.0.0.1");
+            //serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //serverSocket.Bind(new IPEndPoint(ip, myProt));  //绑定IP地址：端口  
+            //serverSocket.Listen(10);    //设定最多10个排队连接请求  
+            //Console.WriteLine("启动监听{0}成功", serverSocket.LocalEndPoint.ToString());
+            ////通过Clientsoket发送数据  
+            //Thread myThread = new Thread(ListenClientConnect);
+            //myThread.Start();
+            //Console.ReadLine();
+        }
+
+        /// <summary>  
+        /// 监听客户端连接  
+        /// </summary>  
+        private static void ListenClientConnect()
+        {
+            while (true)
+            {
+
+                Socket clientSocket = serverSocket.Accept();//**接收一个新客户端的连接请求，clientsocket.connect,没有时阻塞等待，例如 客户端 192.168.5.21 192.168.5.22
+                clientSocket.Send(Encoding.ASCII.GetBytes("Server Say Hello"));
+                Thread receiveThread = new Thread(ReceiveMessage);//每个客户端创建线程进行请求接收并处理
+                
+                receiveThread.Start(clientSocket);
+            }
+        }
+
+        private static void ReceiveMessage2(object clientSocket)
+        {
+            Socket myClientSocket = (Socket)clientSocket;
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                
+            }
+        }
+
+        /// <summary>  
+        /// 接收消息  
+        /// </summary>  
+        /// <param name="clientSocket"></param>  
+        private static void ReceiveMessage(object clientSocket)
+        {
+            Socket myClientSocket = (Socket)clientSocket;
+            while (true)
+            {
+                try
+                {
+                    //通过clientSocket接收数据  
+                    int receiveNumber = myClientSocket.Receive(result);//
+                    Console.WriteLine("接收客户端{0}消息{1}", myClientSocket.RemoteEndPoint.ToString(), Encoding.ASCII.GetString(result, 0, receiveNumber));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    myClientSocket.Shutdown(SocketShutdown.Both);
+                    myClientSocket.Close();
+                    break;
+                }
+            }
+        }
+    }
+}  
---
+++ StringServer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    class StringServer
+    {
+        private static int myProt = 8885;   //端口  
+        private static byte[] result = new byte[1024];
+        public static void InitSocketServer()
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(ip, myProt));  //绑定IP地址：端口  
+            serverSocket.Listen(100);
+
+            //开启线程 接收数据
+            Thread thread = new Thread(ListenConnect);
+            thread.Start(serverSocket);
+        }
+
+        public static void ListenConnect(object serverSocket)
+        {
+            Socket myServerSocket = (Socket)serverSocket;
+            while (true)
+            {
+                Socket mySocket = myServerSocket.Accept();
+                Thread thread = new Thread(ReceiveFile);
+                thread.Start(mySocket);
+
+            }
+        }
+
+
+        public static void ReceiveFile(object mySocket)
+        {
+            Socket myClientSocket = (Socket)mySocket;
+            //读取文件后缀名
+            myClientSocket.Receive(result, 3, 0);
+            string ext = Encoding.UTF8.GetString(result, 0, 3);
+
+            //获取文件大小
+            myClientSocket.Receive(result, 20, SocketFlags.None);
+            int filelength = int.Parse(Encoding.UTF8.GetString(result, 0,20));
+            //获取文件内容
+
+            string path = Environment.CurrentDirectory + "/temp/a." + ext;
+            FileStream fs = File.Create(path);
+            int receivedLength = 0;
+            while (receivedLength < filelength)
+            {
+                int bytes = myClientSocket.Receive(result, result.Length, 0);
+                receivedLength += bytes;
+                fs.Write(result, 0, result.Length);
+            }
+            fs.Flush();
+            fs.Close();
+            Console.WriteLine("接收文件完成");
+            Console.WriteLine("回复客户端接收完成");
+            //回复客户端接收成功
+            result = Encoding.UTF8.GetBytes("yes");
+            myClientSocket.Send(result);
+            Thread.Sleep(10000);
+            myClientSocket.Shutdown(SocketShutdown.Both);
+            myClientSocket.Close();
+
+
+
+
+
+
+        }
+
+    }
+}
